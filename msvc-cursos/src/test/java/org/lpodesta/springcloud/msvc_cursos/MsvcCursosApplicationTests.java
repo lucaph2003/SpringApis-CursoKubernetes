08bc299e437515d11b4b6d6d4c87c7961d9cbaf7
Luca Podesta <lucapodesta47@gmail.com>
2024-11-08 11:22:47 -0300

    Nuevo microservicio de cursos

---
+++ msvc-cursos/src/test/java/org/lpodesta/springcloud/msvc_cursos/MsvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lpodesta.springcloud.msvc_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
